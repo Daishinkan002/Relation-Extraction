5dd1eedfa68e3072eeb420d316c11e94bb9b0811
daishinkan002 <shantanutripathi002@gmail.com>
2022-01-11 22:31:26 +0530

    Ignore code_testing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,20 +8,37 @@ from dotenv import find_dotenv, load_dotenv
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
+
+
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """ 
+    Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+
+def print_list(parent_dir):
+    '''
+    A function to print_list beautifully
+    '''
+    for i, dir in enumerate(parent_dir):
+        print(str(i+1) + ". ", dir)
+    print()
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
+    
     project_dir = Path(__file__).resolve().parents[2]
+    
+    data_dir = Path('../../data/')
+    directories = [x for x in data_dir.iterdir() if x.is_dir()]
+    
+    print_list(directories)
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
